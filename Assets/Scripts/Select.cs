9603a6d1bb58d06e8777ee463019f748e013d028
Mathilde Degris <m.degris@etudiant.ecolemelies.fr>
2021-12-15 14:12:41 +0100

    Add Script Select

--- Assets/Scripts/Select.cs
+++ Assets/Scripts/Select.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Select : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool isSelected = false;
+    [SerializeField] GameObject sceneToClose;
+    [SerializeField] GameObject sceneToOpen;
+
+    public void Selection(InputAction.CallbackContext obj)
+    {
+        // Et si on appuie sur le bouton
+        if (!obj.performed)
+        {
+            if (isSelected == true)
+            {
+                // La scène actuelle se désactive
+                sceneToClose.SetActive(false);
+                // La prochaine scène s'actiuve
+                sceneToOpen.SetActive(true);
+                // Le booléen redevient faux
+                isSelected = false;
+                Debug.Log("c bon");
+                // Si le booléen "isSelected" est vrai
+            }
+        }
+    }
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Si On entre en collision avec l'objet qui porte le tag "objectToFind"
+        if (collision.tag.Equals("objectToFind"))
+        {
+            // Le booléen "isSelected" devient vrai
+            isSelected = true;
+        }
     }
 }
