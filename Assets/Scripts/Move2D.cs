f9f02bcddc98c7006e21a701dd389c3ed29d9b4e
Mathilde Degris <m.degris@etudiant.ecolemelies.fr>
2021-12-15 15:48:17 +0100

    Clean Script Move2D + Add script Move2D to Player

--- Assets/Scripts/Move2D.cs
+++ Assets/Scripts/Move2D.cs
@@ -14,14 +14,12 @@ public class Move2D : MonoBehaviour
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    
-
     private void Update()
     {
         //var directionXY = new Vector2(direction.x, direction.y);
         rb2D.velocity = direction.normalized * speed;
-
     }
+
     public void Move(InputAction.CallbackContext context)
     {
         direction = context.ReadValue<Vector2>();
