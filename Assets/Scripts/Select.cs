df974cb5cc1659391bd47267404bfd65a8fcee42
Mathilde Degris <m.degris@etudiant.ecolemelies.fr>
2021-12-15 15:30:18 +0100

    Add Script SelectObject

--- Assets/Scripts/Select.cs
+++ Assets/Scripts/Select.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Select : MonoBehaviour
+public abstract class Select : MonoBehaviour
 {
-    private bool isSelected = false;
+    [HideInInspector] public bool isSelected = false;
     [SerializeField] GameObject sceneToClose;
     [SerializeField] GameObject sceneToOpen;
 
@@ -12,37 +12,15 @@ public class Select : MonoBehaviour
         // Et si on appuie sur le bouton
         if (!obj.performed)
         {
-            if (isSelected == true)
-            {
-                // La scène actuelle se désactive
-                sceneToClose.SetActive(false);
-                // La prochaine scène s'actiuve
-                sceneToOpen.SetActive(true);
-                // Le booléen redevient faux
-                isSelected = false;
-                Debug.Log("c bon");
-                // Si le booléen "isSelected" est vrai
-            }
+            // La scène actuelle se désactive
+            sceneToClose.SetActive(false);
+            // La prochaine scène s'actiuve
+            sceneToOpen.SetActive(true);
+            // Le booléen redevient faux
+            Debug.Log("c bon");
+            // Si le booléen "isSelected" est vrai
         }
     }
-    
-    void Start()
-    {
-        
-    }
 
-    void Update()
-    {
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        // Si On entre en collision avec l'objet qui porte le tag "objectToFind"
-        if (collision.tag.Equals("objectToFind"))
-        {
-            // Le booléen "isSelected" devient vrai
-            isSelected = true;
-        }
-    }
+    public abstract void OnTriggerEnter2D(Collider2D collision);
 }
